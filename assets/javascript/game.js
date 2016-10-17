442a53c5c8fb6c91afb422d881e1907d0e8721e3
Lisa Williams <williams.lisarenee@gmail.com>
2016-10-17 18:47:26 -0400

    debugging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,19 @@
 // Getting jQuery with it! NaN-NaN-NaN-NaN-NaN-Na! // 
+"use strict";
+
  $(document).ready(function() {
-   	var crystals = ['assets/images/1.png',
-   					'assets/images/2.png',
-   					'assets/images/3.png',
-   					'assets/images/4.png',
-   					'assets/images/5.png',
-   					'assets/images/6.png'];
+ 	var crystals = ['assets/images/1.png',
+ 					'assets/images/2.png',
+ 					'assets/images/3.png',
+ 					'assets/images/4.png',
+ 					'assets/images/5.png',
+ 					'assets/images/6.png',];
+
 	var counter = 0;
 	var wins = 0; 
 	var losses = 0;
+// Global Variables //
+	var numberGuess = 0;
 	$('#win').text(wins);
 	$('#lose').text(losses);
 
@@ -17,12 +22,12 @@
 
 	function newCrystals () {
 		var numbers = []
-		while (numbers.length < 6) {
+		while (numbers.length < crystals.length) {
 			var randomNumber = Math.ceil(Math.random()*12)
 			var found = false;
 			for (var i = 0; i < numbers.length; i++){
 				if (numbers[i] == randomNumber) {
-					found = true; break
+					found = true; break;
 				}
 			}
 			if(!found)numbers[numbers.length]=randomNumber;
@@ -30,13 +35,16 @@
 		console.log(numbers);
 
 		for (i = 0; i < numbers.length; i++) {
-			var imageCrystal = $('img');
-			imageCrystal.attr('data-num', numbers[i]);
-			imageCrystal.addClass('crystalImage')
-			$('crystals').append(imageCrystal);
+			var imageCrystal = $('<img>');
+			imageCrystal.attr('data-num',numbers[i]);
+			imageCrystal.attr('src',crystals[i]);
+			imageCrystal.attr('alt','crystals');
+			imageCrystal.addClass('crystalImage');
+			$('.crystals').append(imageCrystal);
 		}
 	}
 
+// Function to Start Game - Counter starts at 0 //
 function newGame() {
 
 	counter = 0; 
@@ -46,12 +54,17 @@ function newGame() {
 		return Math.floor(Math.random()*(max-min+1)+min);
 	}
 
+//	Variables = random number //
+
 	var numberGuess = randomIntFromInterval(19,120);
 	$('.value').text(numberGuess);
 
-	$('.crystalImage').on('click', function(){
-		counter = counter + parseInt($(this).data('num'));
+// Function to click on Gem // 
 
+	$('.crystalImage').on('click', function(){
+		counter += parseInt($(this).data('num'));
+
+// Code to display the random # to collect // 
 
 		$('#score').text(counter);
 		
